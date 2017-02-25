9813317f3a4c6d0c4f9f300d04221cbed1d82ac6
jacob.nelson23@outlook.com <jacob.nelson23@outlook.com>
2017-02-25 17:59:33 -0600

    Initial Commit

---
+++ blackjack.py
@@ -0,0 +1,19 @@
+from builtins import input
+import time
+
+#Welcoming the player
+name = input("What is your name?")
+
+print ("Hello " +name+ ". Let's play Blackjack!")
+
+print ("")
+
+#wait for one second
+time.sleep(2)
+
+print ("")
+
+
+
+
+	
