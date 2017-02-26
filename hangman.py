2d457d6e655f67756d47f369bf2a8947a186dccb
jacob.nelson23@outlook.com <jacob.nelson23@outlook.com>
2017-02-25 19:50:28 -0600

    Adding second word

--- hangman.py
+++ hangman.py
@@ -1,103 +1,43 @@
 from builtins import input 
 import time
 
-#welcoming the user
 name = input("What is your name? ")
-
 print("Hello, " + name + "  time to play hangman!")
-
 print ("")
 
-#wait for 1 second
 time.sleep(1)
 
 print ("Start guessing...")
 time.sleep(0.5)
 
-#here we set the secret
-word = "dinner"
-
-#creates a variable with an empty value
-guesses = ' '
-
-#determine the number of turns
+word = "Dinner"
+second_word = "Code"
+guesses = ' ' 
 turns = 10
 
-# Create a while loop
+def worldloop (word):
+	while turns > 0:
+		failed = 0
+		for char in (word) or (second_word):
+			if char in guesses:
+				print(char)
+			else:
+				print("_")
+				failed += 1
 
-#check if the turns are more than zero
-while turns > 0:
-	# make a counter that starts with zero
-	failed = 0
-	
-	# for every character in secret_word
-	for char in (word):
-		#see if the caracter is in the players guesses
-		if char in guesses:
-			#print then out the characte
-			print(char)
-		else:
-			# if not found, print a dash
-			print("_")
-			# and increase the failed counter with one
-			failed += 1
-	#if failed is qual to zero
-	# print You Won
-	if failed == 0:
-		print ("You win!")
-		#exit the loop, effectively ending the script
-		break
-		
-	print("")
-	
-	#ask the user go guess a character
-	guess = input ("guess a character:")
-	
-	# set the playerz guess to guesses
-	guesses += guess
-	
-	#if guess is not found in the secret word
-	if guess not in (word):
-		#turns counter descreases with 1 (now 9)
-		turns -= 1
-		print("Wrong")
-		
-		#how many turns are left
-		print("You have", +turns, "more guesses")
-		
-		if turns == 0:
-			#print "You Lose"
-			print("You Lose")
-			
-while turns > 0:
-	# make a counter that starts with zero
-	
-	# print You Won
-	if failed == 0:
-		print ("You Win")
-		#exit the loop, effectively ending the script
-		break
-		
-	print("")
-	
-	#ask the user go guess a character
-	guess = input ("guess a character:")
-	
-	# set the playerz guess to guesses
-	guesses += guess
-	
-	#if guess is not found in the secret word
-	if guess not in (second_word):
-		#turns counter descreases with 1 (now 9)
-		turns -= 1
-		print("Wrong")
-		
-		#how many turns are left
-		print("You have", +turns, "more guesses")
-		
-		if turns == 0:
-			#print "You Lose"
-			print("You Lose")
+		if failed == 0:
+			print ("You win!")
+			break
+		print("")
+
+		guess = input ("guess a character:")
+		guesses += guess
+		if guess not in (word) or (second_word):
+			turns -= 1
+			print("Wrong")
+			print("You have", +turns, "more guesses")		
+			if turns == 0:
+				print("You Lose")
 		
 		
 	
