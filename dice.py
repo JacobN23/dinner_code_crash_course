01c9d85d5b600e710be4de4c19c0f71b9e3787a8
jacob.nelson23@outlook.com <jacob.nelson23@outlook.com>
2017-02-25 18:52:03 -0600

    Added sum of Dice Rolls

--- dice.py
+++ dice.py
@@ -12,13 +12,15 @@ def main():
 	while rolling:
 		roll_again = input ("Ready to roll? ENTER=Roll. Q=Quit")
 		dice_roll = input ("How many times would you like to roll the dice?")
+		roll_sum = 0
 		if roll_again.lower() != "q":
 			times_rolled = 0
 			while times_rolled < int(dice_roll):
 				num_rolled = roll(sides)
 				times_rolled = times_rolled + 1
-			
+				roll_sum += num_rolled
 				print("You rolled a {}".format( num_rolled))
+			print("The sum of this is {}".format( roll_sum))	
 		else:
 			rolling = False
 			
